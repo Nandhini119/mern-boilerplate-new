b6c43e09b55d4666e9fc6e0cf74e722bc008c0bd
nandhini <nandhini.g21@wipro.com>
2017-12-21 10:40:35 +0530

    first commit'

---
+++ webclient/clientapp.jsx
@@ -0,0 +1,26 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const {browserHistory, hashHistory, Route, Router} = require('react-router');
+const login = require('./components/Login/Login.jsx');
+const MainComp = React.createClass({
+  render: function() {
+    return (
+      <div>
+                  <NavBar/>
+                  <br/>
+                  <br/>
+
+                  {this.props.children}
+              </div>
+    );
+  }
+});
+ReactDOM.render(
+  <Router history={hashHistory}>
+
+  {/* <Route path="/phleboHome" component={phleboHome}/>*/}
+  {/* <Route component={MainComp}> */}
+    <Route path="/" component={login}/>
+    {/* <Route path="/logPage" component={logPage}/> */}
+  {/* </Route> */}
+</Router>, document.getElementById('app'));
---
+++ webclient/components/Login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Accordion, Card, Label, Header} from 'semantic-ui-react';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>vfcywedcytfuewuyfewuyfewf</p>
+      </div>
+    );
+  }
+}
+
+module.exports = Login;
